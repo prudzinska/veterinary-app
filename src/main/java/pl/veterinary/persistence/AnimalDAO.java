a089de351dc1ed88a927d34976cd1db37613ceae
pokraka <patrycja.rudzinska1992@gmail.com>
2019-01-01 20:52:01 +0100

    other changes

--- src/main/java/pl/veterinary/persistence/AnimalDAO.java
+++ src/main/java/pl/veterinary/persistence/AnimalDAO.java
@@ -3,10 +3,12 @@ package pl.veterinary.persistence;
 import javax.transaction.Transactional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import pl.veterinary.model.Animal;
 
 @Transactional
+@Repository
 public interface AnimalDAO extends JpaRepository<Animal, Long> {
 
 }
